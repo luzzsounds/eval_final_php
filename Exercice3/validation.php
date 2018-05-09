138fae76557d445bfc296f79645dc589f6925f53
Ludwig <leb.ludwig@gmail.com>
2018-05-09 15:23:27 +0200

    Creation validation.php et getXHR.js

---
+++ Exercice3/validation.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dell
+ * Date: 09/05/2018
+ * Time: 14:47
+ */
+
+include "./classes/Log.php";
+$marque = $_REQUEST['marque'] ?? "";
+$modèle = $_REQUEST['modèle'] ?? "";
+$année = $_REQUEST['année'] ?? "";
+$couleur = $_REQUEST['couleur'] ?? "";
+
+Log::logWrite("Bonjour $marque $modèle dont l'année est $année et la couleur est $couleur");
