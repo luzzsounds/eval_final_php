5a6b6a6f0a6446cadf5fd09cf9d8d10ea3eac26c
Ludwig <leb.ludwig@gmail.com>
2018-05-09 15:50:19 +0200

    COMMIT FINAL....

--- Exercice4/classes/chat.php
+++ Exercice4/classes/chat.php
@@ -1,8 +1,68 @@
 <?php
-/**/
+/*   EXERCICE 4 LUDWIG*/
 
-class chat
+class Chat
 {
 
+    private $prenom;
+
+    private $age;
+
+    private $couleur;
+
+    private $sexe;
+
+    private $race;
+
+	public function __construct($prenom, $age, $couleur, $sexe, $race)
+	{
+		$this->prenom = $prenom;
+		$this->age = $age;
+		$this->couleur = $couleur;
+		$this->sexe = $sexe;
+		$this->race = $race;
+
+	}
+/*--------------------------------------------------------------*/
+
+	public function getPrenom(){
+		return $this->prenom;
+	}
+
+
+	public function getAge(){
+		return $this->age;
+	}
+
+
+	public function getCouleur(){
+		return $this->couleur;
+	}
+
+
+	public function getSexe(){
+		return $this->sexe;
+	}
+
+
+	public function getRace(){
+		return $this->race;
+	}
+
+
+	public function getInfos(){
+
+		 $infos = array(
+			"le nom du chat est ".$this->getPrenom().'.',
+			"son age : ".$this->getAge().'.',
+			"sa couleur est :".$this->getCouleur().'.',
+			"son sexe est ".$this->getSexe().'.',
+			"la race est ".$this->getRace().'.<br>'
+		);
+		return $infos;
+	}
+
 }
+
+?>
 
